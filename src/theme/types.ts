8fe9ec0d415e8d1864437c311b8b193a9977b312
Vijay Shukla <vshukla684@gmail.com>
2025-12-18 12:49:52 +0530

    add basic setup with login and theme

---
+++ src/theme/types.ts
@@ -0,0 +1,11 @@
+export type ThemeMode = "light" | "dark";
+
+export type Theme = {
+  mode: ThemeMode;
+  background: string;
+  surface: string;
+  text: string;
+  subText: string;
+  border: string;
+  primary: string;
+};
